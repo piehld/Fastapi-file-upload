15cc0066e02562d4b2f0dee685adfdf21a192ed1
Dennis Piehl <piehld@gmail.com>
2022-09-28 01:49:35 -0400

    troubleshooting async and multithreading issues

--- client.py
+++ client.py
@@ -2,18 +2,22 @@ import concurrent.futures
 import requests
 import os
 import io
+from datetime import datetime
 import time
+import httpx
+import asyncio
+import aiohttp
 
 chunk_size = 1024  * 1024 * 8
 DEBUG = False
 UPLOAD_URL='http://127.0.0.1:8000/upload'
 PURGE_URL='http://127.0.0.1:8000/purge'
-FILES_TO_UPLOAD = ['/test/directory/5gigfile', 
-'/test/directory/10gigfile',
-'/test/directory/15gigfile',
-'/test/directory/20gigfile']
+FILES_TO_UPLOAD = [
+    '/Users/dennis/rcsb/py-rcsb_app_file/rcsb/app/tests-file/test-data/bigFile.txt.256mb',
+]
 
 def serial_test():
+    print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
     print(f'Uploading {len(FILES_TO_UPLOAD)} files serially')
     results = []
     for file in FILES_TO_UPLOAD:
@@ -21,17 +25,20 @@ def serial_test():
     print("Serial Uploading Result")
     for result in results:
         print(result)
+    print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
 
 def concurrent_test():
+        print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
         print(f'Uploading {len(FILES_TO_UPLOAD)} files with threadpool')
-        with concurrent.futures.ThreadPoolExecutor(max_workers=10) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
             futures = {executor.submit(upload_file, file): file for file in FILES_TO_UPLOAD}
-            results = []
-            for future in concurrent.futures.as_completed(futures):
-                results.append(future.result())
-            print("Multi-threaded multiple files result")
-            for result in results:
-                print(result)
+            # results = []
+            # for future in concurrent.futures.as_completed(futures):
+            #     results.append(future.result())
+            # print("Multi-threaded multiple files result")
+            # for result in results:
+                # print(result)
+        print(datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3])
 
 def human_friendly(bites):
     unit = "B"
@@ -65,8 +72,25 @@ def testing_stats(func):
             return None
     return get_statistics
 
-def async_request(req_body, tmp):
-    return requests.post(UPLOAD_URL,data=req_body, files={'file' : tmp})
+async def gather_async_request(file_path, req_body):
+    tL = []
+    tmp =  io.BytesIO()
+    with requests.Session() as client:
+        with open(file_path, "rb") as to_upload:
+            for i in range(0, req_body["total_chunks"]):
+                req_body["total_chunks_uploaded"]+=1
+                packet_size =min(req_body["file_size"] - (i*chunk_size), chunk_size)
+                req_body["chunk_index"] = i
+                req_body["chunk_byte_offset"] = req_body["chunk_index"] * chunk_size
+                tmp.truncate(packet_size)
+                tmp.seek(0)
+                tmp.write(to_upload.read(packet_size))
+                tmp.seek(0)
+                tL.append(client.post(UPLOAD_URL,data=req_body, files={'file' : tmp}))
+        # res = await asyncio.gather(*tL)
+        # print("res", res)
+        # return res
+        return await asyncio.gather(*tL)
 
 @testing_stats
 def upload_file(file_path):
@@ -86,45 +110,34 @@ def upload_file(file_path):
         "chunk_index" : 0,
         "chunk_byte_offset" : 0,
         "total_chunks" : total_chunks,
+        "total_chunks_uploaded" : 0,
         "file_size" : file_size
     }
 
     if ASYNC:
-        pool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+        pool = concurrent.futures.ThreadPoolExecutor(max_workers=1)  # FASTER WITH "1" instead of "10"?! That pointed out, true "async" should occur only on one thread...
 
     tmp =  io.BytesIO()
-    with open(file_path, "rb") as to_upload:
-        for i in range(0, req_body["total_chunks"]):
-            packet_size =min(req_body["file_size"] - (req_body["chunk_index"]*chunk_size), chunk_size)
-            tmp.truncate(packet_size)
-            tmp.seek(0)
-            tmp.write(to_upload.read(packet_size))
-            tmp.seek(0)
-            if ASYNC:
-               response = pool.submit(async_request,req_body, tmp) 
-            else:
-                response = requests.post(UPLOAD_URL,data=req_body, files={'file' : tmp})
-            if DEBUG and not ASYNC:
-                print(str(response.content))
-            if not ASYNC and response.status_code != 200:
-                break
-            req_body["chunk_index"] = req_body["chunk_index"] + 1
-            req_body["chunk_byte_offset"] = req_body["chunk_index"] * chunk_size
-    return response
+
+    tot= 0
+    total_chunks_uploaded =0
+    response = asyncio.run(gather_async_request(file_path, req_body))
+    # return response
+    return
 
 
 
 if __name__ == "__main__":
-    print('Without ASYNC')
-    ASYNC=False 
-    response = requests.get(PURGE_URL)
-    serial_test() # Each file is uploaded one after another
-    response = requests.get(PURGE_URL) 
-    concurrent_test() # 10 files being uploaded concurrently
+    # print('Without ASYNC')
+    # ASYNC=False 
+    # response = requests.get(PURGE_URL)
+    # serial_test() # Each file is uploaded one after another
+    # response = requests.get(PURGE_URL) 
+    # concurrent_test() # 10 files being uploaded concurrently
 
     print('With ASYNC')
     ASYNC=True # Same file can have upto 10 chunks sent concurrently
-    response = requests.get(PURGE_URL)
-    serial_test()
+    # response = requests.get(PURGE_URL)
+    # serial_test()
     response = requests.get(PURGE_URL)
     concurrent_test()
--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+"""
+Run with `uvicorn main:app`
+
+Then run `python client.py`
+"""
+
 from fastapi import FastAPI, Form, UploadFile, HTTPException
 import glob
 import os
 import logging
+from time import sleep
 
 app = FastAPI()
 log = logging.getLogger(__name__)
@@ -20,6 +27,7 @@ async def upload(file: UploadFile,
     chunk_index : int = Form(...),
     chunk_byte_offset: int = Form(...),
     total_chunks : int = Form(...),
+    total_chunks_uploaded : int = Form(...),
     file_size : int = Form(...) ):
 
     save_path = os.path.join(DATA_DIR, file_name)
@@ -34,8 +42,12 @@ async def upload(file: UploadFile,
     except OSError:
         log.exception('Could not write to file')
         raise HTTPException(status_code=500, detail="Could not write to file")
-    if chunk_index + 1 == total_chunks:
+    print("save_path size: ", os.path.getsize(save_path), "file_size", file_size)
+    print("chunk index / total chunks", chunk_index, total_chunks)
+    print("total_chunks_uploaded / total chunks", total_chunks_uploaded, total_chunks)
+    if total_chunks_uploaded == total_chunks:
         if os.path.getsize(save_path) != file_size:
+            print("BAD save_path size: ", os.path.getsize(save_path), "file_size", file_size)
             log.error(f"File {file_name} was completed, "
                       f"but has a size mismatch."
                       f"Was {os.path.getsize(save_path)} but we"
